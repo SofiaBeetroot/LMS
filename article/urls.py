c6d092be6d7cc5fdc2b34b332ff9c0a7d122592a
Sofia <s.orzhekhovska@gmail.com>
2023-02-18 16:53:08 +0200

    add example of QuerySet

--- article/urls.py
+++ article/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path
-from article.views import get_topic_list
+from article.views import *
 
 urlpatterns = [
     path('topics/', get_topic_list),
+    path('create/', create_topic),
+    path('update/', update_topic),
+    path('delete/', delete_topic)
 ]
--- article/views.py
+++ article/views.py
@@ -1,6 +1,8 @@
+import datetime
+from django.db.models import Count, Avg, Max, Min
 from django.shortcuts import render
 from django.http import HttpResponse
-from article.models import Topic
+from article.models import Topic, User
 
 
 # def index(request):
@@ -10,3 +12,69 @@ from article.models import Topic
 def get_topic_list(request):
     context = {'topic_list': Topic.objects.all()}
     return render(request, 'courses.html', context)
+
+
+# Create - 2 variants
+# Update - 2 variants
+# Delete
+
+def create_topic(request):
+    # I
+    # topic = Topic(title='Django ORM', text='We are learning Django', type=4)
+    # topic.subtitle = 'My subtitle'
+    # topic.save()
+    # II
+    topic = Topic.objects.create(title='Django ORM', text='We are learning Django', type=4)
+    return render(request, 'courses.html', {'topic_list': [topic]})
+
+
+def update_topic(request):
+    # I
+    # topic = Topic.objects.get(id=4)
+    # topic.author = User.objects.get(username='Sofia')
+    # topic.save()
+    data = {'text': 'Django ORM', 'subtitle': 'My subtitle',
+            'url': 'https://docs.djangoproject.com/en/4.1/topics/db/queries/'}
+    # for key, value in data.items():
+    #     setattr(topic, key, value)
+    # topic.save()
+    # II
+    topic = Topic.objects.filter(id=4)
+    topic.update(**data)
+    return render(request, 'courses.html', {'topic_list': topic})
+
+
+def delete_topic(request):
+    # I
+    topic = Topic.objects.get(id=4)
+    topic.delete()
+    # II
+    # Topic.objects.filter(id=4).delete()
+    return render(request, 'courses.html', {'topic_list': Topic.objects.all()})
+
+
+"""Filter"""
+def filter_topic(request):
+    # Get all records in table
+    all_topic = Topic.objects.all()
+    # Get record with conditions
+    topics = Topic.objects.filter(type=5)
+    topics = Topic.objects.filter(type__range=(1, 5))
+    topics = Topic.objects.filter(type=5, title__contains='Django')
+    topics = Topic.objects.filter(author__username__contains='Sofia')
+    topics = Topic.objects.filter(author__username__contains='Sofia').count()
+    topics = Topic.objects.filter(author__username__contains='Sofia').first()
+    topics = Topic.objects.filter(author__username__contains='Sofia').last()
+    topics = Topic.objects.filter(author__username__contains='Sofia').exists()
+    topics = Topic.objects.filter(author__username__contains='Sofia').distinct('type')
+    topics = Topic.objects.filter(author__username__contains='Sofia').order_by('creation_date').distinct('type')
+    topics = Topic.objects.filter(author__username__contains='Sofia').order_by('-creation_date').distinct('type')
+    topics = Topic.objects.exclude(url__isnull=True)
+    topics = Topic.objects.filter(type=5).exclude(author_id=3)
+    now_ = datetime.datetime.now()
+    topics = Topic.objects.filter(type=4).union(
+        Topic.objects.filter(creation_date__range=(now_ - datetime.timedelta(days=7), now_ - datetime.timedelta(days=2))))
+    # topics = Topic.objects.annotate('type')
+    # topics = Topic.objects.annotate(Count('author'))
+    # topics = Topic.objects.aggregate(Max('text__len'))
+    return render(request, 'courses.html', {'topic_list': topics})
